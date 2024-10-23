98dd64d26f1c63092107bcbff103347143a88981
Andrew C. Scoppa <andrew-scoppa@github.com>
2024-10-23 14:00:08 -0400

    Update

--- company-library/tests/Developer.test.js
+++ company-library/tests/Developer.test.js
@@ -1,6 +1,12 @@
 const Developer = require('../src/Developer');
 
 /*
+TODO: Implement the test cases and the assertions. Make sure to use the correct assertion function.
+TODO: What additional test cases could be implemented?
+*/
+
+
+/*
 Test case 1:
 Create a Developer object with the ID 1, name 'John Smith', salary 60000, and programming language 'JavaScript'. Verify that the developer has been created successfully
 
@@ -9,9 +15,8 @@ Create a Developer object with the ID 1, name 'John Smith', salary 60000, and pr
 
 Test case 3:
 Create a Developer object with the ID 1, name 'John Smith', salary 60000, and programming language 'JavaScript'. Verify that the developer name can be retrieved successfully
+
+Test case 4:
+Create a Developer object with the ID 1, name 'John Smith', salary 60000, and programming language 'JavaScript'. Verify that the developer salary can be retrieved successfully
 */
 
-/*
-TODO: Implement the test cases and the assertions. Make sure to use the correct assertion function.
-TODO: What additional test cases could be implemented?
-*/
